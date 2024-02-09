ac098815031da15d86d6f79a794653dea1c48042
slepec <slepec2018@gmail.com>
2024-02-09 15:09:31 +0000

    add_mainpage

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,12 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
+  safelist: [
+    "hover:border-blue-300",
+    "hover:border-green-500",
+    "hover:border-red-500",
+  ]
 };
